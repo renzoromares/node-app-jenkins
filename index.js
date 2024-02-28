2f7b37e79fb00c93e99f8eaf73dd8260e836d013
Renzo Romares <68291583+renzoromares@users.noreply.github.com>
2024-02-28 18:03:06 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World 1234')
+  res.send('Hello World 12345')
 })
 
 app.listen(3000)
